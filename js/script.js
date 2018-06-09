ff6e9f4580b604dcc9190dd81ccaae5cd6f265db
Lisa9740 <barretalison@gmail.com>
2018-06-09 20:42:01 +0400

    Changement fond body

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+//pour animation hover sous le menu
+var angle = 0;
+function galleryspin(sign) { 
+spinner = document.querySelector("#spinner");
+if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
+
 // pour animation texte dans le header//
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
