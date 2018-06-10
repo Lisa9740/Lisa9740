c0a093c6fd1f78507ae10b4b8a942946c86afe21
Lisa9740 <barretalison@gmail.com>
2018-06-10 11:26:01 +0400

    Ajout animation zoom portfolio

--- js/script.js
+++ js/script.js
@@ -1,105 +1,106 @@
-$(".navbar-toggler").click(function(){
+$(".navbar-toggler").click(function () {
   $(".navbar-collapse").css("background", "#f3eeee");
 })
 
 
 // pour animation texte dans le header//
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
+  if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
+  } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
+  if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
-    }
+  }
 
-    setTimeout(function() {
+  setTimeout(function () {
     that.tick();
-    }, delta);
+  }, delta);
 };
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+window.onload = function () {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
 
 // pour changer le background quand il y a scrolling 
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-          $(".no-bg").css("background" , "url('./css/img/trianglify.png')");
-           $(".no-bg").css("box-shadow" , "0 1px 6px rgba(0, 0, 0, 0.274)");
-           $("#totop").css("transform", "rotate(360deg)");
-   
-        }else if (scroll > 0 && scroll < 300) {
-          $(".no-bg").css("background" , "white");
-          $("#totop").css("transform", "rotate(180deg)");
-        }else if (scroll == 0){
-          $("#totop").fadeIn("slow", 0.33);
-          $(".no-bg").css("background" , "url('./css/img/trianglify.png')");
-          $(".no-bg").css("box-shadow" , "0 0px 0px rgba(0, 0, 0, 0.274)");
-        }
-        else{
-      
-            $(".no-bg").css("background" , "transparent");
-            $("#totop").css("display", "block");  	
-         
-        }
-    })
-    
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $(".no-bg").css("background", "url('./css/img/trianglify.png')");
+      $(".no-bg").css("box-shadow", "0 1px 6px rgba(0, 0, 0, 0.274)");
+      $("#totop").css("transform", "rotate(360deg)");
+
+    } else if (scroll > 0 && scroll < 300) {
+      $(".no-bg").css("background", "white");
+      $("#totop").css("transform", "rotate(180deg)");
+    } else if (scroll == 0) {
+      $("#totop").fadeIn("slow", 0.33);
+      $(".no-bg").css("background", "transparent");
+      $(".no-bg").css("box-shadow", "0 0px 0px rgba(0, 0, 0, 0.274)");
+    } else {
+
+      $(".no-bg").css("background", "transparent");
+      $("#totop").css("display", "block");
+
+    }
   })
 
-  // pour un "smooth anchor scolling"
+})
 
-  $('a[href*="#"]')
+// pour un "smooth anchor scolling"
+
+$('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .not('[href="#carouselExampleIndicators"]')
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
@@ -111,7 +112,7 @@ $(document).ready(function(){
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000, function() {
+        }, 1000, function () {
           // Callback after animation
           // Must change focus!
           var $target = $(target);
@@ -119,7 +120,7 @@ $(document).ready(function(){
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
@@ -127,40 +128,57 @@ $(document).ready(function(){
     }
   });
 // partie parours 
-(function() {
+(function () {
 
-    'use strict';
-  
-    // define variables
-    var items = document.querySelectorAll(".timeline li");
-  
-    // check if an element is in viewport
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-  
-    function callbackFunc() {
-      for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
-          items[i].classList.add("in-view");
-        }
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
       }
     }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+
+})();
+
+
+
+
+// (function () {
+// var angle = 0;
+//   if ($("#right").click(function() {
+//     angle = angle + 45; 
+//   })); else {
+//     angle = angle - 45;
+//   }
   
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
+//   $("#spinner").css( "-webkit-transform", "rotateY(" +
+//   angle + "deg);" );
   
-
-  })();
-
-
-
+   
+//   $("#spinner").css( "transform", "rotateY(" +
+//   angle + "deg);" );
+   
+//   })();
