8b5c3a5c5c877f345839cdb1f07a9f5d7c5e889c
Lisa9740 <barretalison@gmail.com>
2018-06-10 10:42:02 +0400

    modification du scrolling

--- js/script.js
+++ js/script.js
@@ -70,12 +70,13 @@ $(document).ready(function(){
           $(".no-bg").css("background" , "url('./css/img/trianglify.png')");
            $(".no-bg").css("box-shadow" , "0 1px 6px rgba(0, 0, 0, 0.274)");
            $("#totop").css("transform", "rotate(360deg)");
+   
         }else if (scroll > 0 && scroll < 300) {
           $(".no-bg").css("background" , "white");
-         
+          $("#totop").css("transform", "rotate(180deg)");
         }else if (scroll == 0){
           $("#totop").fadeIn("slow", 0.33);
-          $("#totop").css("transform", "rotate(180deg)");
+          $(".no-bg").css("background" , "url('./css/img/trianglify.png')");
           $(".no-bg").css("box-shadow" , "0 0px 0px rgba(0, 0, 0, 0.274)");
         }
         else{
@@ -90,24 +91,41 @@ $(document).ready(function(){
 
   // pour un "smooth anchor scolling"
 
-  function scrollNav() {
-    $('.anchor a').click(function(){  
-      //Toggle Class
-      $(".active").removeClass("active");      
-      $(this).closest('li').addClass("active");
-      var theClass = $(this).attr("class");
-      $('.'+theClass).parent('li').addClass('active');
-      //Animate
-      $('html, body').stop().animate({
-          scrollTop: $( $(this).attr('href') ).offset().top - 160
-      }, 400);
-      return false;
-    });
-    $('.scrollTop a').scrollTop();
-  }
-  scrollNav();
-  
-
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 // partie parours 
 (function() {
 
